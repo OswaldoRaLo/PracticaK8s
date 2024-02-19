29bed36706f92cef1f6b02cadf3c867cdeaa7fee
OswaldoRaLo <90931465+OswaldoRaLo@users.noreply.github.com>
2024-02-19 00:32:12 -0600

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM python:alpine
+
+COPY conexion.py /app/conexion.py
+
+RUN pip install mysql-connector-python
+
+WORKDIR /app
+
+CMD ["python", "conexion.py"]
+
---
+++ conexion.py
@@ -0,0 +1,26 @@
+#api que conecta con todo
+import mysql.connector
+
+config = {
+    'host': 'mysql-prk8',  
+    'user': 'root',
+    'password': '1234',
+    'database': 'mysql_docker',
+    'port': 3306,
+}
+
+connection = mysql.connector.connect(**config)
+
+try:
+    cursor = connection.cursor()
+
+    query = "SELECT * FROM nombre_tabla"
+    cursor.execute(query)
+    results = cursor.fetchall()
+
+    for row in results:
+        print(row)
+
+finally:
+    cursor.close()
+    connection.close()
